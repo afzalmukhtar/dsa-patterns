21c3dbbc9c69835ae0fe9ebfdd5fcfbf9077ca44
Afzal Mukhtar <afzalmukhtar985@gmail.com>
2026-03-07 18:52:01 +0000

    Update Valid Palindrome with user implementation

--- 01-two-pointers/problems/125-valid-palindrome.py
+++ 01-two-pointers/problems/125-valid-palindrome.py
@@ -1,33 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        """
-        Valid Palindrome using the Two-Pointer technique.
-        
-        Args:
-            s (str): The input string to check.
-            
-        Returns:
-            bool: True if the string is a palindrome, False otherwise.
-        """
         L = 0
         R = len(s) - 1
 
         while L < R:
-            # Skip non-alphanumeric characters from the left
-            # Using .isalnum() is crucial as it includes numbers (0-9)
-            while L < R and not s[L].isalnum(): 
-                L += 1
-            
-            # Skip non-alphanumeric characters from the right
-            while L < R and not s[R].isalnum(): 
-                R -= 1
+            while not s[L].isalnum() and L < R: L += 1
+            while not s[R].isalnum() and L < R: R -= 1
 
-            # Compare characters after converting to lowercase
             if s[L].lower() != s[R].lower():
                 return False
-            
-            # Move pointers inward
-            L += 1
-            R -= 1
-                
+            else:
+                L += 1
+                R -= 1
+
         return True
