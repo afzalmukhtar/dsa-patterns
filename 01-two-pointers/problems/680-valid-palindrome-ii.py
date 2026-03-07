770d2bc4dc40828b4741cc31a3a684b7b48fef9e
Afzal Mukhtar <afzalmukhtar985@gmail.com>
2026-03-07 19:34:05 +0000

    Update Valid Palindrome II with latest implementation

--- 01-two-pointers/problems/680-valid-palindrome-ii.py
+++ 01-two-pointers/problems/680-valid-palindrome-ii.py
@@ -1,21 +1,22 @@
 class Solution:
+
     def isPal(self, s: str, L: int, R: int):
         while L < R:
-            if s[L] == s[R]:
-                L += 1
-                R -= 1
-            else:
+            if s[L] != s[R]:
                 return False
+            L += 1
+            R -= 1
         return True
 
+            
     def validPalindrome(self, s: str) -> bool:
         L = 0 
         R = len(s) - 1
 
+        
         while L < R:
-            if s[L] == s[R]:
-                L += 1
-                R -= 1
-            else:
+            if s[L] != s[R]:
                 return self.isPal(s, L + 1, R) or self.isPal(s, L, R - 1)
+            L += 1
+            R -= 1
         return True
