f1142a71395666b16959e87dcf11699d43f89149
Afzal Mukhtar <afzalmukhtar985@gmail.com>
2026-03-07 19:32:38 +0000

    Update Valid Palindrome II with user's implementation

--- 01-two-pointers/problems/680-valid-palindrome-ii.py
+++ 01-two-pointers/problems/680-valid-palindrome-ii.py
@@ -1,24 +1,21 @@
 class Solution:
-    def validPalindrome(self, s: str) -> bool:
-        """
-        Valid Palindrome II using Two-Pointer technique.
-        Allows for at most one character deletion.
-        """
-        def is_pal(left: int, right: int) -> bool:
-            while left < right:
-                if s[left] != s[right]:
-                    return False
-                left += 1
-                right -= 1
-            return True
+    def isPal(self, s: str, L: int, R: int):
+        while L < R:
+            if s[L] == s[R]:
+                L += 1
+                R -= 1
+            else:
+                return False
+        return True
 
-        L, R = 0, len(s) - 1
+    def validPalindrome(self, s: str) -> bool:
+        L = 0 
+        R = len(s) - 1
 
         while L < R:
-            if s[L] != s[R]:
-                # If mismatch found, try skipping either the left or the right character
-                return is_pal(L + 1, R) or is_pal(L, R - 1)
-            L += 1
-            R -= 1
-            
+            if s[L] == s[R]:
+                L += 1
+                R -= 1
+            else:
+                return self.isPal(s, L + 1, R) or self.isPal(s, L, R - 1)
         return True
